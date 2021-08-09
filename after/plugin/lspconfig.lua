59d84d844a65ce9222f26c5ff536ea3becf6ea63
Micah Halter <micah@balena.io>
2021-08-09 14:59:53 -0400

    Added diagnostic publishing
    Signed-off-by: Micah Halter <micah@balena.io>

--- after/plugin/lspconfig.lua
+++ after/plugin/lspconfig.lua
@@ -81,3 +81,14 @@ nvim_lsp.cssls.setup{
 	on_attach = on_attach,
 	cmd = { 'css-languageserver', '--stdio' }
 }
+
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+	vim.lsp.diagnostic.on_publish_diagnostics, {
+		underline = true,
+		-- This sets the spacing and the prefix, obviously.
+		virtual_text = {
+			spacing = 4,
+			prefix = ''
+		}
+	}
+)
